5d56a701042572279d1b682e6f0e245916aae4e0
Kai Xuen Kok <G00451291@atu.ie>
2025-12-18 19:29:59 +0800

    Initial project setup with MVVM folders and Movie model

---
+++ MovieExplorer/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExplorer.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string[] Genre { get; set; }
+        public string Director { get; set; }
+
+        [System.Text.Json.Serialization.JsonPropertyName("IMDB rating")]
+        public double ImdbRating { get; set; }
+
+        [System.Text.Json.Serialization.JsonPropertyName("Emoji representing the primry genre")]
+        public string Emoji { get; set; }
+
+    }
+}
